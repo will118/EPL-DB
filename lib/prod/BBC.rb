ca6aebc559ebd06e4efbcb563e875e8dd64fa93a
Will Bartlett <will118@gmail.com>
2013-10-16 10:27:46 +0100

    wnb tings

--- lib/prod/BBC.rb
+++ lib/prod/BBC.rb
@@ -1,39 +1,50 @@
 class BBC
+	include BBCTeamNameNormaliser
 
 	attr_reader :statsjson
 
-	def initialize(team)
+	def initialize
 		@team = team
-		@rawlink = get_bbc(team)
 	end
 
 	def is_it_time
 		 Fixture.order(:kickoff).first(8).each do |x| 
         time_until = x.kickoff - Time.now 
-        if time_until < 180
+        if !!(time_until < 180) && !!(x.jsonurl != nil ) 
          BBC.recorder(x)
-        elsif time_until < 1800
-          # Try and get teams 
-        else
-          "Stil a bit to go.."
+        elsif !!(time_until < 1800) && !!(x.rawlink == nil)
+          get_bbc 
+        else time_until
         end
       end
 	end
 
 	def get_bbc
-		Fixture.order(:kickoff).first(8).each do |x|
 
-			uri = "http://www.bbc.co.uk/sport/football/premier-league/fixtures"
-			doc = Nokogiri::HTML(open("#{uri}"))
-		  doc1 = doc.xpath('html/body/div[3]/div/div/div[1]/div[3]/div[2]/div')
-		  mentions = doc1.search "[text()*='#{@team.titleize}']"
-		  match = mentions.first.parent.parent.parent.parent
-			url = match.css('a').last['href']
-			#  Line here to check its an actual link.
+		Fixture.order(:kickoff).first(8).each do |fixture|
+			if fixture.rawlink == nil
+				team = TeamNameNormaliser(fixture.hometeam)
+				uri = "http://www.bbc.co.uk/sport/football/premier-league/fixtures"
+				doc = Nokogiri::HTML(open("#{uri}"))
+			  doc1 = doc.xpath('html/body/div[3]/div/div/div[1]/div[3]/div[2]/div')
+			  mentions = doc1.search "[text()*='#{team.titleize}']"
+			  match = mentions.first.parent.parent.parent.parent
+				@rawlink = match.css('a').last['href']
 
-			#  I need a line here to save to the original Active Record relation not just @obj unless that would somehow magically work, but I doubt it.
-			url
-		
+				if is_valid_match? == true
+					raw_link
+					fixture.rawlink = @rawlink
+					fixture.jsonurl = @jsonurl
+					fixture.lineup_url = @lineup_url
+					fixture.save
+				else
+					"Much too early"
+				end
+			else
+				@rawlink = fixture.rawlink 
+				@jsonurl = fixture.jsonurl
+				@lineup_url = fixture.lineup_url
+			end
 	end
 
 
@@ -42,60 +53,69 @@ class BBC
 	end
 
 	def raw_link
-		if is_valid_match? == true
 			driver = Selenium::WebDriver.for(:remote, :url => "http://localhost:9134")
 			driver.navigate.to @rawlink
 			page = driver.page_source
 			json_link = page.match(/(http:\/\/polling.bbc.co.uk\/sport\/shared\/football\/oppm\/json).{11}/)
 			lineup_link = page.match(/(http:\/\/polling.bbc.co.uk\/sport\/shared\/football\/oppm\/line-up).{11}/)
-			# Likewise I need another line here to save these.
-			@finalurl = json_link.to_s
+			@jsonurl = json_link.to_s
 			@lineup_url = lineup_link.to_s
 			driver.quit
-		else "Too early"
-		end
 	end
 
-	def get_json
-		rawbbc = JSON.parse HTTParty.get(@finalurl).response.body.delete('(').delete(');')
+	def get_json(jsonurl)
+		rawbbc = JSON.parse HTTParty.get(jsonurl).response.body.delete('(').delete(');')
 		midway = rawbbc['data']['payload']['Match']
 		result = []
 		midway.each { |x| result = x.assoc('stats') }
-		@statsjson = result[1]
+		@result[1]
 	end
 
 
 	def recorder(ar_record)
-		# say this as "AR record" not "Activerecord record" if an enrique ever reads this.
-		ar_rec = ar_record
-    data = @statsjson
+		# say this as "AR record" not "Activerecord record" in case an enrique-type ever comes across this.
+		x = ar_record
+		x.hometeam
+		x.awayteam
+
+    data = get_json(x.jsonurl)
 
     if data['shotsOnTarget'] == nil
     	"No data yet"
  		else
 	    poss = Poss.where(:team => @team).create
 	    poss.homeposs = data['possession']['home']
+	    poss.hometeam = x.hometeam
 	    poss.awayposs = data['possession']['away']
+	    poss.awayteam = x.awayteam
 	    poss.save
 
 	    targets = Target.where(:team => @team).create
 	    targets.homeshots = data['shotsOnTarget']['home']
+	    targets.hometeam = x.hometeam
 	    targets.awayshots = data['shotsOnTarget']['away']
+	    targets.awayteam = x.awayteam
 	    targets.save
 
 	    shots = Shot.where(:team => @team).create
 	    shots.homeshots = data['shots']['home']
+	    shots.hometeam = x.hometeam
 	    shots.awayshots = data['shots']['away']
+	    shots.awayteam = x.awayteam
 	    shots.save
 
 	    corners = Corner.where(:team => @team).create
 	    corners.home = data['corners']['home']
+	    corners.hometeam = x.hometeam
 	    corners.away = data['corners']['away']
+	    corners.awayteam = x.awayteam
 	    corners.save
 
 	    fouls = Foul.where(:team => @team).create
 	    fouls.home = data['fouls']['home']
+	    fouls.hometeam = x.hometeam
 	    fouls.away = data['fouls']['away']
+	    fouls.awayteam = x.awayteam
 	    fouls.save
 	  end
 	end
@@ -129,55 +149,4 @@ class BBC
 	    awayxi.save 
     end   
 	end
-
-  def self.recorder(jsonurl, team)
-
-		rawbbc = JSON.parse HTTParty.get(jsonurl).response.body.delete('(').delete(');')
-		midway = rawbbc['data']['payload']['Match']
-		result = []
-		midway.each { |x| result = x.assoc('stats') }
-		statsjson = result[1]
-	
-    data = statsjson
-
-    if data['shotsOnTarget'] == nil
-    	"No data yet"
- 		else
-	    poss = Poss.where(:team => team).create
-	    poss.homeposs = data['possession']['home']
-	    poss.awayposs = data['possession']['away']
-	    poss.save
-
-	    targets = Target.where(:team => team).create
-	    targets.homeshots = data['shotsOnTarget']['home']
-	    targets.awayshots = data['shotsOnTarget']['away']
-	    targets.save
-
-	    shots = Shot.where(:team => team).create
-	    shots.homeshots = data['shots']['home']
-	    shots.awayshots = data['shots']['away']
-	    shots.save
-
-	    corners = Corner.where(:team => team).create
-	    corners.home = data['corners']['home']
-	    corners.away = data['corners']['away']
-	    corners.save
-
-	    fouls = Foul.where(:team => team).create
-	    fouls.home = data['fouls']['home']
-	    fouls.away = data['fouls']['away']
-	    fouls.save
-	  end
-  end
-
-  def self.tester(rawlink)
-		  driver = Selenium::WebDriver.for(:remote, :url => "http://localhost:9134")
-			driver.navigate.to rawlink
-			page = driver.page_source
-			json_link = page.match(/(http:\/\/polling.bbc.co.uk\/sport\/shared\/football\/oppm\/json).{11}/)
-			lineup_link = page.match(/(http:\/\/polling.bbc.co.uk\/sport\/shared\/football\/oppm\/line-up).{11}/)
-			json_link.to_s
-	end
-
-		
 end
