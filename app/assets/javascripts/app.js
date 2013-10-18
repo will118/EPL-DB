784e474fde0b776789ae0b5afa8d3a9c015df69f
Will Bartlett <will118@gmail.com>
2013-10-18 01:27:13 +0100

    Cleaned up assets

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -82,31 +82,33 @@ d3App.controller('AppCtrl', function AppCtrl ($scope, $http) {
 
 	
 	$scope.colours = [
-    {'Arsenal': 'red'},
-    {'Chelsea': 'blue'},
-    {'West Bromwich Albion': 'black'},
-    {'Aston Villa': 'purple'},
-    {'Liverpool': 'red'},
-    {'Tottenham Hotspur': 'light grey'},
-    {'Manchester United': 'red'},
-    {'Stoke City': 'red'},
-    {'Cardiff': 'red'},
-    {'West Ham United': 'purple'},
-    {'Arsenal': 'red'},
-    {'Chelsea': 'blue'},
-    {'Arsenal': 'red'},
-    {'Chelsea': 'blue'},
-    {'Arsenal': 'red'},
-    {'Chelsea': 'blue'},
-    {'Arsenal': 'red'},
-    {'Chelsea': 'blue'},
-    {'Fulham': 'black'}
+    {'Arsenal': '#e8000b'},
+    {'Chelsea': '#063381'},
+    {'West Bromwich Albion': '#090c41'},
+    {'Aston Villa': '#5a0029'},
+    {'Liverpool': '#c3001e'},
+    {'Tottenham Hotspur': '#051246'},
+    {'Manchester United': '#ce000e'},
+    {'Stoke City': '#d62331'},
+    {'Cardiff City': '#e5001e'},
+    {'West Ham United': '#4c172d'},
+    {'Crystal Palace': '#06347c'},
+    {'Southampton': '#e5002e'},
+    {'Newcastle United': '#666666'},
+    {'Sunderland': '#e30021'},
+    {'Norwich City': '#14993e'},
+    {'Everton': '#073b8b'},
+    {'Manchester City': '#4db1e6'},
+    {'Swansea City': '#b4b4b4'},
+    {'Hull City': '#f79616'},
+    {'Fulham': '#000000'}
   ];
 
 	$scope.colourman = function () {
 	for (var i = 0; i < ($scope.colours).length; i++) {
 	  if ($scope.colours[i][$scope.team]) {
-	  		$scope.myColour = $scope.colours[i][$scope.team]	
+	  		$scope.myColour = $scope.colours[i][$scope.team]
+	  		console.log($scope.colours[i][$scope.team]);	
 	  	} 
 		}
 	};
