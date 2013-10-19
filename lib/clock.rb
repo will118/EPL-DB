83175facd8d64e03bab47af78a31e82f0f829c5e
Will Bartlett <will118@gmail.com>
2013-10-19 13:09:57 +0100

    the big change is in the live testing ive done to fix scraping

--- lib/clock.rb
+++ lib/clock.rb
@@ -7,7 +7,8 @@ include Clockwork
 handler do |job|
  puts "running the scheduled job #{job}."
 end
-
+ 
+ 
 # every(1.hours, 'Team Form'){`rake populater:teamform`}
 # every(3.hours, 'Opta Text'){`rake populater:optatext`}
 # every(4.hours, 'Fixture Schedule'){`rake populater:schedule`}
