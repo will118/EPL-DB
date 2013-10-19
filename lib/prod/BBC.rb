27f957801d426f14433d0450a0dce34c8218cd8b
Will Bartlett <will118@gmail.com>
2013-10-19 15:57:44 +0100

    why wont it work, what is different..?

--- lib/prod/BBC.rb
+++ lib/prod/BBC.rb
@@ -36,10 +36,6 @@ class BBC
 				doc = Nokogiri::HTML(open("#{uri}"))
 				doc1 = doc.xpath('html/body/div[3]/div/div/div[1]/div[3]/div[2]/div')
 				mentions = doc1.search "[text()*='#{team}']"
-				if mentions == nil
-					puts "its-nil"
-					next
-				end
 					match = mentions.first.parent.parent.parent.parent
 					@rawlink = match.css('a').last['href']
 					if is_valid_match? == true
