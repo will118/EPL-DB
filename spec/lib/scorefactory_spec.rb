30e02dd6d3db1e6296470d281a10d12673266b06
Will Bartlett <will118@gmail.com>
2013-11-03 13:53:10 +0000

    Deleted old score method, working on new one

--- spec/lib/scorefactory_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe ScoreFactory do
-
-  it "saves scores to the database" do
-  	# well the problem is it only saves live scores and thats all I want to test.  
-  end
-
-end
