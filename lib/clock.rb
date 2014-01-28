f8398e93a1f250466b93cbaedf5337721d186f20
Will Bartlett <will118@gmail.com>
2014-01-28 17:36:59 +0000

    Moved JS around for rails compilation and added keepalive for redis keys

--- lib/clock.rb
+++ lib/clock.rb
@@ -8,9 +8,10 @@ handler do |job|
  puts "running the scheduled job #{job}."
 end
 
-
 every(1.hours, 'Team Form'){`rake populater:teamform`}
 every(1.minute, 'Scores'){RemoteAPI.api_save}
+every(1.minute, 'Fix Countdown'){ApiScore.fixtures_countdown}
+every(1.minute, 'League Table'){ApiScore.league_table}
 every(12.hours, 'Opta Text'){`rake populater:optatext`}
 every(4.hours, 'Fixture Schedule'){`rake populater:schedule`}
 every(6.hours, 'Squawka Scraper'){`rake populater:squawka`}
